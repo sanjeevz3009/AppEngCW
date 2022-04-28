764201cfb2778515d3b557fbe4867a74a4e79a4a
UP2002235 <91495869+UP2002235@users.noreply.github.com>
2022-04-28 23:17:14 +0100

    Auth0 implemented.

---
+++ auth_config.js
@@ -0,0 +1,5 @@
+export default {
+    domain: "dev-7e4vicc6.us.auth0.com",
+    clientId: "a7dCA5wvKLf3jgmvBbP6bjoYnkEPXbAt",
+    audience: "http://localhost:8080",
+}
--- svr.js
+++ svr.js
@@ -1,5 +1,10 @@
 import express from "express";
 import * as br from "./bricksResult.mjs";
+import path from "path";
+import url from "url";
+
+import authConfig from "./auth_config.js";
+import auth0Helpers from './auth0_helpers.js';
 
 const app = express();
 app.use(express.static("client"));
@@ -39,6 +44,31 @@ function putBrick(req, res) {
     const brick = br.updateBrickQuantity(req.body);
     res.json(brick);
 }
+
+// serve the auth config publicly
+app.get('/auth_config', (req, res) => {
+  res.json(authConfig);
+});
+
+const auth0 = auth0Helpers(authConfig);
+
+// protect /api from unauthenticated users
+app.use('/api', auth0.checkJwt);
+
+app.get('/api/hello', async (req, res) => {
+  const userId = auth0.getUserID(req);
+
+  // load the user information, in production this would need caching or storing in a database
+  const profile = await auth0.getProfile(req);
+
+  res.send(`Hello user ${userId}, here's your profile:\n${JSON.stringify(profile, null, 2)}`);
+
+  console.log('successful authenticated request by ' + userId);
+});
+
+// this will serve the files present in static/ inside this stage
+app.use(express.static(path.join(path.dirname(url.fileURLToPath(import.meta.url)), '../static')));
+
 
 app.get("/bricks", getBricks);
 app.get("/bricks/:id", getBrick);
